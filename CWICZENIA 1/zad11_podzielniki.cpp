20a1e3ff0708c246fc6c1b68ea1ad83daf6509eb
Maciej_Sikora <maciejsikora2302@gmail.com>
2019-04-01 15:55:41 +0200

    Create zad11_podzielniki.cpp

---
+++ CWICZENIA 1/zad11_podzielniki.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=sqrt(n);i++){
+        if(n%i==0){
+            cout<<i<<" "<<n/i<<endl;
+        }
+    }
+}
